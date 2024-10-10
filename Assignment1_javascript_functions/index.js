aee895bbb6d49a44304fa8cb94928e31b3b81983
Caboyd33 <cidneyboyd@yahoo.com>
2024-10-09 22:50:08 -0500

    Add files via upload

---
+++ Assignment1_javascript_functions/index.js
@@ -0,0 +1,17 @@
+let myFunction = function () {
+
+}
+
+
+
+function printGreeting (number1, number2) {
+  let a = 10
+  let b= 20
+  return a + b
+}
+
+
+
+
+
+let printGreeting2 = (number1, number2) => {console.log (number1*number2)}
